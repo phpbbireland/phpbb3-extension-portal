f68958639ca00b7f459e01b6f71856d6f3c39c09
Michael O'Toole <phpbbireland@gmail.com>
2015-04-29 08:03:49 +0100

    Missing global added

--- blocks/block_top_posters.php
+++ blocks/block_top_posters.php
@@ -3,7 +3,7 @@
 *
 * Kiss Portal extension for the phpBB Forum Software package.
 *
-* @copyright (c) 2014 Michael O’Toole <http://www.phpbbireland.com>
+* @copyright (c) 2014 Michael Oâ€™Toole <http://www.phpbbireland.com>
 * @license GNU General Public License, version 2 (GPL-2.0)
 *
 */
@@ -16,7 +16,7 @@ if (!defined('IN_PHPBB'))
 	exit;
 }
 
-global $k_config, $k_blocks, $user;
+global $k_config, $k_blocks, $user, $phpEx;
 
 foreach ($k_blocks as $blk)
 {
